35740d52aa8cf46613247b7ca025df6cc1a860e1
Pratham16112002 <prathamdhiman@rediffmail.com>
2022-06-12 15:38:36 +0530

    Insertion Sort

--- Arrays/Sorting.java
+++ Arrays/Sorting.java
@@ -30,10 +30,21 @@ public class Sorting {
             arr[i]=temp; 
         }
     }
+    public static void InsertionSort(int arr[]){
+        for(int i = 0 ; i<arr.length ; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
     public static void main(String args[]){
         int arr[] = {7,8,3,1,2};
         // Calling the bubble sort function 
-        SelectionSort(arr);
+        InsertionSort(arr);
         // Calling the print function 
         printArray(arr);
         
