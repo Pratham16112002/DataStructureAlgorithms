e6793b31a6623f33c6a1c5b43c9a5f930f5ffdc2
Pratham16112002 <prathamdhiman@rediffmail.com>
2022-06-12 14:21:09 +0530

    Folder

---
+++ Arrays/RcBinarySearch.java
@@ -0,0 +1,29 @@
+package Code1.Arrays;
+
+import java.util.Scanner;
+
+public class RcBinarySearch {
+	public static boolean binarySearch(int marks[] , int low , int high , int key) {
+		while(low<=high && low<=marks.length -1 ) {
+			int mid=low + ( high - low)/2;
+			if(key==marks[mid])return true;
+			if(key<marks[mid])return binarySearch(marks,low,mid-1,key);
+			return binarySearch(marks,mid+1,high,key);
+			
+		}
+		return false;		
+	}
+	public static void main(String args[]) {
+//		Take an input from the user and search and search for a specific element in it 
+		Scanner sc = new Scanner(System.in);
+				 
+		int[] marks = new int[10];
+		for(int i = 0 ; i<3 ; i++) {
+			marks[i]=sc.nextInt();
+		}
+		System.out.println("Enter the element you want to find ?");
+		int x = sc.nextInt();
+		System.out.println(binarySearch(marks,0,2,x));
+		
+	}
+}
---
+++ Arrays/Sorting.java
@@ -0,0 +1,28 @@
+package Code1.Arrays;
+
+public class Sorting {
+    public static void printArray(int arr[]){
+        for(int i = 0 ; i<arr.length  ; i++ ){
+            System.out.print(arr[i]);
+        }
+    }
+    public static void bubbleSort(int arr[]){
+        for(int i=0 ; i<arr.length ; i++){
+            for(int j = 0 ; j<arr.length -i -1 ; j++ ){
+                if(arr[j]<arr[j+1]){
+                    // Swap the Elements
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {7,8,1,2,1,2};
+        // Calling the bubble sort function 
+        bubbleSort(arr);
+        
+    }
+
+}
