7a3ebdb10ef8e517bdfbc8b0f152f8881caa19a8
Pratham16112002 <prathamdhiman@rediffmail.com>
2022-06-12 14:34:07 +0530

    Final Commit

--- Arrays/Sorting.java
+++ Arrays/Sorting.java
@@ -1,13 +1,13 @@
 public class Sorting {
     public static void printArray(int arr[]){
         for(int i = 0 ; i<arr.length  ; i++ ){
-            System.out.print(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
     public static void bubbleSort(int arr[]){
-        for(int i=0 ; i<arr.length ; i++){
-            for(int j = 0 ; j<arr.length -i -1 ; j++ ){
-                if(arr[j]<arr[j+1]){
+        for(int i=0 ; i<arr.length-1 ; i++){
+            for(int j = 0 ; j<arr.length-i-1 ; j++ ){
+                if(arr[j]>arr[j+1]){
                     // Swap the Elements
                     int temp = arr[j];
                     arr[j]=arr[j+1];
@@ -17,7 +17,7 @@ public class Sorting {
         }
     }
     public static void main(String args[]){
-        int arr[] = {7,8,1,2,1,2};
+        int arr[] = {7,8,3,1,2};
         // Calling the bubble sort function 
         bubbleSort(arr);
         // Calling the print function 
